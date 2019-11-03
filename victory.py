401e2829ba5e508055b7042dd1ed16e88eaf158f
sneggorod2019 <sneggorod@mail.ru>
2019-11-03 13:07:13 +0300

    Добавил модуль 5

---
+++ victory.py
@@ -0,0 +1,6 @@
+grp = 1799
+age_Pushkin = float(input('введите год рождения Пушкина: '))
+while age_Pushkin != grp:
+     age_Pushkin = float(input('введите год рождения Пушкина: '))
+print("правильно")
+print('end')
