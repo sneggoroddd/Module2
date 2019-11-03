60075b95902c494055c4ab7c3ce001695ad87b3e
sneggorod2019 <sneggorod@mail.ru>
2019-11-03 22:37:05 +0300

    Создал викторину

--- victory.py
+++ victory.py
@@ -1,35 +1,31 @@
 again = "Да"
 while again == 'Да':
-    grp: float = 1799
+    age_Pushkin =int(input('введите год рождения Пушкина: '))
+    age_Holms = float(input('введите год рождения Шерлока Холмса: '))
+    age_Lermontov = float(input('введите год рождения Лермонтова: '))
+    age_Esenin = float(input('введите год рождения Есенина: '))
+    age_Gagarin = float(input('введите год рождения Гагарина: ')
+    grp = 1799
     grh: float = 1854
     grl: float = 1814
     gre: float = 1895
     grg: float = 1934
-    tra = 0
-    fla = 5
-    age_Pushkin = float(input('введите год рождения Пушкина: '))
-        If  age_Pushkin == grp:
-        tra = tra + 1
-    age_Holms = float(input('введите год рождения Шерлока Холмса: '))
-    age_Lermontov = float(input('введите год рождения Лермонтова: '))
-    age_Esenin = float(input('введите год рождения Есенина: '))
-    age_Gagarin = float(input('введите год рождения Гагарина: '))
-    If  age_Pushkin == grp:
-         tra = tra + 1
-    If  age_Holms == grh:
-         tra = tra + 1
-    If  age_Lermontov == grl:
-         tra = tra + 1
-    If  age_Esenin == gre:
-         tra = tra + 1
-    If  age_Gagarin == grg:
-         tra = tra + 1
-    fla = 5 - tra
+    fla: float = 0
+    if  age_Pushkin != grp:
+         fla = fla + 1
+    elif age_Holms !=  grh:
+         fla = fla + 1
+    elif age_Lermontov !=  grl:
+         fla = fla + 1
+    elif age_Esenin !=  gre:
+         fla = fla + 1
+    else age_Gagarin !=  grg:
+         fla = fla + 1
+    tra = 5 - fla
     ptra = tra / 5 * 100
     pfla = fla / 5 * 100
-    Print ("правильных ответов :" , tra , ptra)
-    Print ("Не правильных ответов ;" , fla , pfla)
+    Print ("правильных ответов :" , tra, ptra)
+    Print ("Не правильных ответов ;" , fla, pfla)
     again = (input('Попробовать еще раз? '))
-
 print("Всего хорошего")
 print('end')
